8f9259aa39180d37e1fcb5deef1f5beee3559d7a
Emma Ryan <emma.c.ryan@aol.com>
2020-08-16 11:01:37 -0400

    Updated BQF class and AERISBQ factoring function (apologies for the delay!); changes in syntax.

--- AERISBQ/AERISBQ_Factor.m
+++ AERISBQ/AERISBQ_Factor.m
@@ -1,18 +1,13 @@
 function [p, q] = AERISBQ_Factor(N)
-    v1 = BQF;
-	v2 = BQF;
     i = 1; %%Analagous to row number of matrix. 
     
-    v1.value = zeros(i,4);
-    v2.value = zeros(i,4);
-    
-    BQF_v = {v1, v2};
+    v1 = zeros(i,4);
     
     %%Initializing with values obtained Round 0.
-    v1.value(1,1:4) = BQF.BQF_construction(2, 1, 1, N);
+    v1 = BQF.BQF_construction(2, 1, 1, N);
    
-    r(i) = v1.value(i, 2);
-    s(i) = v1.value(i, 3);
+    r(i) = v1(i, 2);
+    s(i) = v1(i, 3);
     
     total_forms = 0;
     round_number = 0;
@@ -24,42 +19,71 @@ function [p, q] = AERISBQ_Factor(N)
         round_number = round_number + 1;
         final_v1 = size(v1);
         i = final_v1(1) + 1;
-        v2.value = zeros(i,4);
+        v2 = zeros(i,4);
         
         %%Calculate new child BQFs from the previously added BQFs in v1.
-        previous_round_i = v1(1, :) == 2^(round_number - 1);
-        previous_i_change = size(previous_round_i); 
-        previous_i_change = previous_i_change(1);
-        delta_i_previous = (i - 1) - previous_i_change; 
+        if round_number == 1
+            previous_i_change = 1;
+        else
+            previous_round_i = v1(:, 1) == 2^(round_number - 1);
+            previous_i_change = size(previous_round_i); 
+            previous_i_change = previous_i_change(1);
+        end
+        delta_i_previous = (i - 1) - previous_i_change + 1; 
         
-        a = 2.^(round_number + 1);
+        a = 2^(round_number);
         
-        if v1(1, :) == 2^(round_number - 1)
+        while round_number == 1
+            for k = 1:(i - 1)
+                BQF_children_round_i = BQF.BQF_create_children(a, r(k), s(k), N);
+                BQF_children_round_i_final(1:4, 1:4) = BQF_children_round_i;
+            end
+            %%Input newly formed BQFs into v2 and remove permutations.
+            v2(1:4, 1:4) = BQF_children_round_i_final(1:4, 1:4);
+        
+            %%test code > hardcode v2 with v2 notepad values (quick test - with
+            %%if not working, comment out remove_permutations
+            %%v2 = BQF.BQF_remove_permutations(v2, 4);       
+            
+            %%Adding new BQFs (with removed permutations) into v1. 
+            size_v2 = size(v2);
+            size_v2 = size_v2(1);
+            v1(i:(i + size_v2 - 1), 1:4) = v2(1:size_v2, 1:4);
+        
+            total_forms = total_forms + size_v2;
+        
+            delta_i_current = (i + size_v2) - i;
+        end
+        
+        while v1(:, 1) == 2^(round_number - 1)
             w = 1;
             
             for k = ((i - 1) - delta_i_previous):(i - 1)
-                BQF_children_round_i = BQF.BQF_create_children(a, r(k), s(k), N);
+                BQF_children_round_i = BQF.BQF_create_children(a, r(k), s(k), N, round_number);
                 BQF_children_round_i_size = size(BQF_children_round_i);
                 BQF_children_round_i_size = BQF_children_round_i_size(1);
-                BQF_children_round_i_final(w:(w + (BQF_children_round_i_size)), 1:4) = BQF_children_round_i;
-                w = w + BQF_children_round_i_size + 1;
+                BQF_children_round_i_final(w:(w + (BQF_children_round_i_size) - 1), 1:4) = BQF_children_round_i;
+                w = w + BQF_children_round_i_size;
             end
+            
+            %%Input newly formed BQFs into v2 and remove permutations.
+            v2(1:w, 1:3) = BQF_children_round_i_final(1:w, 1:3);
+        
+            %%test code > hardcode v2 with v2 notepad values (quick test - with
+            %%if not working, comment out remove_permutations
+            %%v2 = BQF.BQF_remove_permutations(v2, w);       
+            
+            %%Adding new BQFs (with removed permutations) into v1. 
+            size_v2 = size(v2);
+            size_v2 = size_v2(1);
+            v1(i:(i + size_v2 - 1), 1:4) = v2(1:size_v2, 1:4);
+        
+            total_forms = total_forms + size_v2;
+        
+            delta_i_current = (i + size_v2) - i;
         end
-            
-        %%Input newly formed BQFs into v2 and remove permutations.   
-        v2.value(1:w, 1:3) = BQF_children_round_i_final(1:w, 1:3);
-        v2.value = BQF.BQF_remove_permutations(v2, w);       
-            
-        %%Adding new BQFs (with removed permutations) into v1. 
-        size_v2 = size(v2);
-        size_v2 = size_v2(1);
-        v1.value(i:(i + size_v2), 1:3) = v2(:, 1:3);
-        
-        total_forms = total_forms + size_v2;
-        
-        delta_i_current = (i + size_v2) - i;   
-        
-        if v1(1, :) == 2^(round_number + 1)  
+
+        if v1(:, 1) == 2^(round_number - 1)  
             for z = i:(i + delta_i_current)
                 p = v1(1, z) + v1(z, 2);  
             
--- AERISBQ/BQF.m
+++ AERISBQ/BQF.m
@@ -1,23 +1,22 @@
 classdef BQF
-    properties
-        value
-    end
     methods(Static)
-        function [val1, val2, val3, val4] = BQF_reduce(val1, val2, val3, val4)
+        function BQF_reduce_output = BQF_reduce(val1, val2, val3, val4)
             g = gcd(abs(val1), gcd(abs(val2), gcd(abs(val3), abs(val4))));
             val1 = val1/g;
             val2 = val2/g;
             val3 = val3/g;
             val4 = val4/g;
+            BQF_reduce_output = [val1, val2, val3, val4];
         end
         
-        function [val1, val2, val3, val4, val5] = BQF_reduce_2(val1, val2, val3, val4, val5)
+        function BQF_reduce_2_output = BQF_reduce_2(val1, val2, val3, val4, val5)
             g = gcd(abs(val1), gcd(abs(val2), gcd(abs(val3), gcd(abs(val4), abs(val5)))));
             val1 = val1/g;
             val2 = val2/g;
             val3 = val3/g;
             val4 = val4/g;
             val5 = val5/g;
+            BQF_reduce_2_output = [val1, val2, val3, val4, val5]; 
         end
         
         function BQF_output = BQF_construction(a, r, s, n)
@@ -27,90 +26,99 @@ classdef BQF
         end
         
         function out_thisBQF = BQF_create_child(a, r, s, b, c, n)
-            a = int64(a); r = int64(r); s = int64(s); n = int64(n);
+            thisBQF = BQF.BQF_construction(a, (r + b), (s + c), n);
             
             %%Step 1: Skip each trivial rejection
             num = n - r * s - b * c - b * s - c * r;
-            thisBQF = BQF_construction(a, (r + b), (s + c), n);
-            if mod(num, thisbQF(1)) ~= 0
-                ~thisBQF;
-                
+            if mod(num, thisBQF(1)) ~= 0
                 out_thisBQF = [];
+            else
+                n1 = num/thisBQF(1);
+            
+                %%Step 2: U-V substitution
+                a2 = thisBQF(1);
+                b2 = thisBQF(3);
+                c2 = thisBQF(2);
+                n2 = n1;
+                thisBQF = BQF.BQF_reduce(a2, b2, c2, n2);
+            
+                %%Step 3: L substitutions
+                a3 = -thisBQF(1);
+                b3 = thisBQF(1);
+                c3 = -2 * (thisBQF(2) - thisBQF(3));
+                d3 = 2 * (thisBQF(2) + thisBQF(3));
+                n3 = 4 * thisBQF(4);
+                thisBQF = BQF.BQF_reduce_2(a3, b3, c3, d3, n3); 
+            
+                %%Step 4: Reduction
+                a4 = 4 * thisBQF(1);
+                b4 = -4 * thisBQF(1);
+                c4 = -2 * thisBQF(3);
+                d4 = thisBQF(3) + thisBQF(4);
+                n4 = thisBQF(5);
+                thisBQF = BQF.BQF_reduce_2(a4, b4, c4, d4, n4);
+            
+                g = gcd(n4, gcd(abs(thisBQF(1)), abs(thisBQF(3))));
+                a4 = thisBQF(1)/g;
+                c4 = thisBQF(3)/g;
+                n4 = thisBQF(5)/g;
+                thisBQF = [a4, b4, c4, d4, n4];
+            
+                if thisBQF(5) < 0
+                    error('n is a negative value.')
+                end
+            
+                out_thisBQF = [a, (r + b), (s + c), n];
             end
-            n1 = num/thisBQF(1);
-            
-            %%Step 2: U-V substitution
-            a2 = thisBQF(1);
-            b2 = thisBQF(3);
-            c2 = thisBQF(2);
-            n2 = n1;
-            [a2, b2, c2, n2] = BQF_reduce(a2, b2, c2, n2);
-            thisBQF = [a2, b2, c2, n2];
-            
-            %%Step 3: L substitutions
-            a3 = -thisBQF(1);
-            b3 = thisBQF(1);
-            c3 = -2 * (thisBQF(2) - thisBQF(3));
-            d3 = 2 * (thisBQF(2) + thisBQF(3));
-            n3 = 4 * thisBQF(4);
-            [a3, b3, c3, d3, n3] = BQF_reduce_2(a3, b3, c3, d3, n3);
-            thisBQF = [a3, b3, c3, d3, n3];
-            
-            %%Step 4: Reduction
-            a4 = 4 * thisBQF(1);
-            b4 = -4 * thisBQF(1);
-            c4 = -2 * thisBQF(3);
-            d4 = thisBQF(3) + thisBQF(4);
-            n4 = thisBQF(5);
-            [a4, b4, c4, d4, n4] = BQF_reduce_2(a4, b4, c4, d4, n4);
-            thisBQF = [a4, b4, c4, d4, n4];
-            
-            g = GCD(n4, GCD(abs(thisBQF(1)), abs(thisBQF(3))));
-            a4 = thisBQF(1)/g;
-            c4 = thisBQF(3)/g;
-            n4 = thisBQF(5)/g;
-            thisBQF = [a4, b4, c4, d4, n4];
-            
-            if thisBQF(5) < 0
-                error('n is a negative value.')
-            end
-            
-            out_thisBQF = [a, (r + b), (s + c), n];
             
             %%Change the index of vector list- do this in the AERISBQ code
             %%itself.
         end
         
         function out_theseBQFs = BQF_create_children(a, r, s, n)
-            out_theseBQFs(1, 1:4) = BQF_create_child(a, r, s, 0, 0, n);
-            out_theseBQFs(2, 1:4) = BQF_create_child(a, r, s, a, a, n);
-            out_theseBQFs(3, 1:4) = BQF_create_child(a, r, s, a, 0, n);
-            out_theseBQFs(4, 1:4) = BQF_create_child(a, r, s, 0, a, n);
-            
-            for i = 1:4
-                if isempty(out_theseBQFs(i, 1:4)) == true
-                    out_theseBQFs(i, :) = [];
+            out_theseBQFs_1 = BQF.BQF_create_child(a, r, s, 0, 0, n);
+                if isempty(out_theseBQFs_1) == 1
+                    ~out_theseBQFs_1;
+                else
+                    out_theseBQFs_1(1, 1:4) = BQF.BQF_create_child(a, r, s, 0, 0, n);
                 end
-            end
+            out_theseBQFs_2 = BQF.BQF_create_child(a, r, s, a, a, n);
+                if isempty(out_theseBQFs_2) == 1
+                    ~out_theseBQFs_2;
+                else
+                    out_theseBQFs_2(1, 1:4) = BQF.BQF_create_child(a, r, s, a, a, n);
+                end
+            out_theseBQFs_3 = BQF.BQF_create_child(a, r, s, a, 0, n);
+                if isempty(out_theseBQFs_3) == 1
+                    ~out_theseBQFs_3;
+                else
+                    out_theseBQFs_3(1, 1:4) = BQF.BQF_create_child(a, r, s, a, 0, n);
+                end
+            out_theseBQFs_4 = BQF.BQF_create_child(a, r, s, 0, a, n);
+                if isempty(out_theseBQFs_4) == 1
+                    ~out_theseBQFs_4;
+                else
+                    out_theseBQFs_4(1, 1:4) = BQF.BQF_create_child(a, r, s, 0, a, n);
+                end
+           
+           out_theseBQFs = [out_theseBQFs_1, out_theseBQFs_2, out_theseBQFs_3, out_theseBQFs_4];
+           size_out_theseBQFs = length(out_theseBQFs)/4;
+           out_theseBQFs = reshape(out_theseBQFs, [4, size_out_theseBQFs]);
         end
         
-        function fixed_BQFs_final = BQF_remove_permutations(v2, l)
-            r(l) = zeros(l, 3);
-            s(l) = zeros(l, 3);
+        function fixed_BQFs_final = BQF_remove_permutations(v2, w)
+            dups = v2(1:w, 1:3);
             
-            dups = v2(1:l, 1:3);
-            
-            for l = 1:dups(end) 
-                r(l) = dups(l, 2);
-                s(l) = dups(l, 3);
-                if r(l) > s(l)
-                    swap(r(l), s(l));
-                    dups(l, 2) = r(l);
-                    dups(l, 3) = s(l);
+            for w = 1:dups(end) 
+                r = dups(w, 2);
+                s = dups(w, 3);
+                if r > s
+                    r = dups(w, 2);
+                    s = dups(w, 3);
                 end
                 
-                if dups(l, 2) == dups(:, 2) && dups(l, 3) == dups(:, 3)
-                    dups(l, :) = [];
+                if r == dups(:, 2) && s == dups(:, 3)
+                    dups(w, :) = [];
                 end
             end
             
